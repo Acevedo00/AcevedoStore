107e2472ab9b59ed7d0b7bc7feacf62cf51d29e4
Adriel Acevedo <adriex123@gmail.com>
2022-10-27 18:32:46 -0300

    agregue javascript al proyecto

---
+++ script.js
@@ -0,0 +1,16 @@
+//Calcular edad promedio de personas registradas:
+
+const nombreComprador = prompt('Ingresa tu nombre');
+const edad1 = parseInt(prompt('Ingresa tu edad'));
+const edad2 = parseInt(prompt('Ingresa la edad de tu padre'));
+const edad3 = parseInt(prompt('Ingresa la edad de tu madre'));
+
+const promedio = (edad1 + edad2 + edad3) / 3;
+
+console.log(promedio);
+
+
+
+console.log('El nombre del comprador es: ' + nombreComprador);
+
+
