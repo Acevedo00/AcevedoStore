5cd01ff4978b731fa4d9915820721f09791401f6
Adriel Acevedo <adriex123@gmail.com>
2022-10-30 12:10:11 -0300

    agregue un sistema de ecommerce hecho con javascript

--- script.js
+++ script.js
@@ -1,16 +1,68 @@
-//Calcular edad promedio de personas registradas:
-
-const nombreComprador = prompt('Ingresa tu nombre');
-const edad1 = parseInt(prompt('Ingresa tu edad'));
-const edad2 = parseInt(prompt('Ingresa la edad de tu padre'));
-const edad3 = parseInt(prompt('Ingresa la edad de tu madre'));
-
-const promedio = (edad1 + edad2 + edad3) / 3;
-
-console.log(promedio);
+let total = 0;
+let totalMostrar;
+let eleccion;
 
 
+const producto1 = 150900;
+const producto2 = 108299;
+const producto3 = 232499;
 
-console.log('El nombre del comprador es: ' + nombreComprador);
 
+alert("Bienvenido/a! a Acevedo Store!");
+
+
+
+function comprar() {
+
+  do {
+    eleccion = parseInt(
+      prompt(`Seleccioná el producto que vas a comprar:
+    
+    1- Samsung S22FE = $150.900.
+    
+    2- Xbox One S = $108.299.
+    
+    3- Sony PlayStation 5 = $232.499.
+
+    `)
+    );
+  } while (eleccion !== 1 && eleccion !== 2 && eleccion !== 3);
+
+
+
+  if (eleccion === 1) {
+    total += producto1;
+    totalString = String(total);
+    totalMostrar = totalString.slice(0, 3) + "." + totalString.slice(3, 7);
+    alert("Tenes que pagar: $" + totalMostrar);
+  } else if (eleccion === 2) {
+    total += producto2;
+    totalString = String(total);
+    totalMostrar = totalString.slice(0, 3) + "." + totalString.slice(3, 7);
+    alert("Tenes que pagar: $" + totalMostrar);
+  } else {
+    total += producto3;
+    totalString = String(total);
+    totalMostrar = totalString.slice(0, 3) + "." + totalString.slice(3, 7);
+    alert("Tenes que pagar: $" + totalMostrar);
+  }
+
+
+
+  do {
+    eleccion = parseInt(
+      prompt(`Selecciona una opcion:
+    1- Quiero seguir comprando.
+    2- No quiero seguir comprando`)
+    );
+  } while (eleccion !== 1 && eleccion !== 2);
+  if (eleccion === 1) {
+    comprar();
+  } else {
+    totalString = String(total);
+    alert("Hasta luego, pase por caja y pague: $" + totalMostrar);
+  }
+}
+
+comprar();
 
