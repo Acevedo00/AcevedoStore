208aef5948efcd9ed39dca8438dc52f556fd3920
Adriel Acevedo <adriex123@gmail.com>
2022-11-13 23:06:00 -0300

    agregue todos los requisitos para la 2da preentrega

--- script.js
+++ script.js
@@ -1,68 +1,80 @@
-let total = 0;
-let totalMostrar;
-let eleccion;
-
-
-const producto1 = 150900;
-const producto2 = 108299;
-const producto3 = 232499;
-
 
 alert("Bienvenido/a! a Acevedo Store!");
 
 
 
-function comprar() {
-
-  do {
-    eleccion = parseInt(
-      prompt(`Seleccioná el producto que vas a comprar:
-    
-    1- Samsung S22FE = $150.900.
-    
-    2- Xbox One S = $108.299.
-    
-    3- Sony PlayStation 5 = $232.499.
-
-    `)
-    );
-  } while (eleccion !== 1 && eleccion !== 2 && eleccion !== 3);
-
-
-
-  if (eleccion === 1) {
-    total += producto1;
-    totalString = String(total);
-    totalMostrar = totalString.slice(0, 3) + "." + totalString.slice(3, 7);
-    alert("Tenes que pagar: $" + totalMostrar);
-  } else if (eleccion === 2) {
-    total += producto2;
-    totalString = String(total);
-    totalMostrar = totalString.slice(0, 3) + "." + totalString.slice(3, 7);
-    alert("Tenes que pagar: $" + totalMostrar);
-  } else {
-    total += producto3;
-    totalString = String(total);
-    totalMostrar = totalString.slice(0, 3) + "." + totalString.slice(3, 7);
-    alert("Tenes que pagar: $" + totalMostrar);
+class Celular {
+  constructor(nombre, precio, stock, disponibilidad) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+    this.disponibilidad = disponibilidad;
   }
-
-
-
-  do {
-    eleccion = parseInt(
-      prompt(`Selecciona una opcion:
-    1- Quiero seguir comprando.
-    2- No quiero seguir comprando`)
-    );
-  } while (eleccion !== 1 && eleccion !== 2);
-  if (eleccion === 1) {
-    comprar();
-  } else {
-    totalString = String(total);
-    alert("Hasta luego, pase por caja y pague: $" + totalMostrar);
+  cambiarStock() {
+    this.stock = --this.stock;
+  }
+  configurarStock(stock) {
+    this.stock = stock;
   }
 }
 
-comprar();
+const celular1 = new Celular("Iphone 14", 1000, 200, true);
+const celular2 = new Celular("Xiaomi Redmi8", 200, 200, true);
+const celular3 = new Celular("Samsung S22", 900, 200, true);
 
+let total = 0;
+const carrito = [];
+let barrera = true;
+let seguirComprando = true;
+
+function keepShopping() {
+  if (seguirComprando === false) {
+    alert(`El total de tu compra es de $${total}.`);
+    alert(`Gracias por tu compra.`);
+
+    barrera = false;
+  }
+}
+
+function alerta(celular, total, carrito) {
+  alert(`
+    Compraste: ${celular.nombre} x $${celular.precio}. El total de tu compra es de: $${total}. 
+        
+        
+  Lista de productos comprados: ${carrito}
+  `);
+}
+
+while (barrera === true) {
+  let producto = parseInt(
+    prompt(`Ingresa el producto que vas a comprar:
+    1. Iphone
+    2. Xiaomi
+    3. Samgung`)
+  );
+  if (producto === 1) {
+    carrito.push(celular1.nombre);
+    total = total + celular1.precio;
+
+    alerta(celular1, total, carrito);
+
+    seguirComprando = confirm("Queres seguir comprando?");
+    keepShopping();
+  } else if (producto === 2) {
+    carrito.push(celular2.nombre);
+    total = total + celular2.precio;
+
+    alerta(celular2, total, carrito);
+
+    seguirComprando = confirm("Queres seguir comprando?");
+    keepShopping();
+  } else if (producto === 3) {
+    carrito.push(celular3.nombre);
+    total = total + celular3.precio;
+
+    alerta(celular3, total, carrito);
+
+    seguirComprando = confirm("Queres seguir comprando?");
+    keepShopping();
+  }
+}
